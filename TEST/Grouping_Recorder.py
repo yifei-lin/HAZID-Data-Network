aed3a8f70ef249112ea1b54a14ba106e3c16ca6a
yifei-lin <75653726+yifei-lin@users.noreply.github.com>
2024-05-08 17:46:27 +1000

    Delete TEST directory

--- TEST/Grouping_Recorder.py
+++
@@ -1,8 +0,0 @@
-
-
-class GroupingRecorder:
-    def __init__(self):
-        self.grouping_operations = []
-
-    def record(self, node1_id, node2_id, new_group_id):
-        self.grouping_operations.append((node1_id, node2_id, new_group_id))
